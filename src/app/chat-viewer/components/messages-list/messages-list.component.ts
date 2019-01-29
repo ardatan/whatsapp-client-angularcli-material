Urigo/WhatsApp-Clone-Client-Angular

Step 7.2: Remove messages and chats

src/app/chat-viewer/components/messages-list/messages-list.component.ts
@@ -1,14 +1,17 @@
 import {Component, Input} from '@angular/core';
 import {GetChat} from '../../../../types';
+import {SelectableListDirective} from 'ngx-selectable-list';
 
 @Component({
   selector: 'app-messages-list',
   template: `
     <mat-list>
       <mat-list-item *ngFor="let message of messages">
-        <app-message-item [item]="message" [isGroup]="isGroup"></app-message-item>
+        <app-message-item [item]="message" [isGroup]="isGroup"
+                          appSelectableItem></app-message-item>
       </mat-list-item>
     </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
   `,
   styleUrls: ['messages-list.component.scss'],
 })
@@ -20,5 +23,5 @@ export class MessagesListComponent {
   @Input()
   isGroup: boolean;
 
-  constructor() {}
+  constructor(public selectableListDirective: SelectableListDirective) {}
 }

src/app/chat-viewer/containers/chat/chat.component.ts
@@ -12,7 +12,10 @@ import {GetChat} from '../../../../types';
       <div class="title">{{ name }}</div>
     </app-toolbar>
     <div class="container">
-      <app-messages-list [items]="messages" [isGroup]="isGroup"></app-messages-list>
+      <app-messages-list [items]="messages" [isGroup]="isGroup"
+                         appSelectableList="multiple_press" (multiple)="deleteMessages($event)">
+        <app-confirm-selection #confirmSelection></app-confirm-selection>
+      </app-messages-list>
       <app-new-message (newMessage)="addMessage($event)"></app-new-message>
     </div>
   `,
@@ -46,5 +49,9 @@ export class ChatComponent implements OnInit {
 
   addMessage(content: string) {
     this.chatsService.addMessage(this.chatId, content).subscribe();
+  }
+
+  deleteMessages(messageIds: string[]) {
+    this.chatsService.removeMessages(this.chatId, this.messages, messageIds).subscribe();
   }
 }

src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -5,7 +5,7 @@ import {GetChats} from '../../../../types';
   selector: 'app-chat-item',
   template: `
     <div class="chat-row">
-        <div class="chat-recipient" (click)="selectChat()">
+        <div class="chat-recipient">
           <img *ngIf="chat.picture" [src]="chat.picture" width="48" height="48">
           <div>{{ chat.name }} [id: {{ chat.id }}]</div>
         </div>
@@ -18,11 +18,4 @@ export class ChatItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
   chat: GetChats.Chats;
-
-  @Output()
-  select = new EventEmitter<string>();
-
-  selectChat() {
-    this.select.emit(this.chat.id);
-  }
 }

src/app/chats-lister/components/chats-list/chats-list.component.ts
@@ -1,14 +1,17 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {GetChats} from '../../../../types';
+import {SelectableListDirective} from 'ngx-selectable-list';
 
 @Component({
   selector: 'app-chats-list',
   template: `
     <mat-list>
       <mat-list-item *ngFor="let chat of chats">
-        <app-chat-item [item]="chat" (select)="selectChat($event)"></app-chat-item>
+        <app-chat-item [item]="chat"
+                       appSelectableItem></app-chat-item>
       </mat-list-item>
     </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
   `,
   styleUrls: ['chats-list.component.scss'],
 })
@@ -17,12 +20,5 @@ export class ChatsListComponent {
   @Input('items')
   chats: GetChats.Chats[];
 
-  @Output()
-  select = new EventEmitter<string>();
-
-  constructor() {}
-
-  selectChat(id: string) {
-    this.select.emit(id);
-  }
+  constructor(public selectableListDirective: SelectableListDirective) {}
 }

src/app/chats-lister/containers/chats/chats.component.ts
@@ -28,9 +28,13 @@ import {Router} from '@angular/router';
       </button>
     </mat-menu>
 
-    <app-chats-list [items]="chats$ | async" (select)="goToChat($event)"></app-chats-list>
+    <app-chats-list [items]="chats$ | async"
+                    appSelectableList="both"
+                    (single)="goToChat($event)" (multiple)="deleteChats($event)" (isSelecting)="isSelecting = $event">
+      <app-confirm-selection #confirmSelection></app-confirm-selection>
+    </app-chats-list>
 
-    <button class="chat-button" mat-fab color="primary">
+    <button *ngIf="!isSelecting" class="chat-button" mat-fab color="primary">
       <mat-icon aria-label="Icon-button with a + icon">add</mat-icon>
     </button>
   `,
@@ -38,6 +42,7 @@ import {Router} from '@angular/router';
 })
 export class ChatsComponent implements OnInit {
   chats$: Observable<GetChats.Chats[]>;
+  isSelecting = false;
 
   constructor(private chatsService: ChatsService,
               private router: Router) {
@@ -49,5 +54,11 @@ export class ChatsComponent implements OnInit {
 
   goToChat(chatId: string) {
     this.router.navigate(['/chat', chatId]);
+  }
+
+  deleteChats(chatIds: string[]) {
+    chatIds.forEach(chatId => {
+      this.chatsService.removeChat(chatId).subscribe();
+    });
   }
 }

ardatan/whatsapp-client-angularcli-material

src/app/chat-viewer/components/messages-list/messages-list.component.ts
@@ -1,14 +1,17 @@
 import {Component, Input} from '@angular/core';
 import {GetChat} from '../../../../graphql';
+import {SelectableListDirective} from 'ngx-selectable-list';
 
 @Component({
   selector: 'app-messages-list',
   template: `
     <mat-list>
       <mat-list-item *ngFor="let message of messages">
-        <app-message-item [item]="message" [isGroup]="isGroup"></app-message-item>
+        <app-message-item [item]="message" [isGroup]="isGroup"
+                          libSelectableItem></app-message-item>
       </mat-list-item>
     </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
   `,
   styleUrls: ['messages-list.component.scss'],
 })
@@ -20,5 +23,5 @@ export class MessagesListComponent {
   @Input()
   isGroup: boolean;
 
-  constructor() {}
+  constructor(public selectableListDirective: SelectableListDirective) {}
 }

src/app/chat-viewer/containers/chat/chat.component.ts
@@ -13,7 +13,10 @@ import {GetChat} from '../../../../graphql';
       <div class="title">{{ name }}</div>
     </app-toolbar>
     <div class="container">
-      <app-messages-list [items]="messages" [isGroup]="isGroup"></app-messages-list>
+      <app-messages-list [items]="messages" [isGroup]="isGroup"
+                         libSelectableList="multiple_press" (multiple)="deleteMessages($event)">
+        <app-confirm-selection #confirmSelection></app-confirm-selection>
+      </app-messages-list>
       <app-new-message (newMessage)="addMessage($event)"></app-new-message>
     </div>
   `,
@@ -47,5 +50,9 @@ export class ChatComponent implements OnInit {
 
   addMessage(content: string) {
     this.chatsService.addMessage(this.chatId, content).subscribe();
+  }
+
+  deleteMessages(messageIds: string[]) {
+    this.chatsService.removeMessages(this.chatId, this.messages, messageIds).subscribe();
   }
 }

src/app/chats-lister/components/chat-item/chat-item.component.ts
@@ -4,7 +4,7 @@ import {GetChats} from '../../../../graphql';
 @Component({
   selector: 'app-chat-item',
   template: `
-    <div class="chat-row" (click)="selectChat()">
+    <div class="chat-row">
       <img class="chat-pic" [src]="chat.picture || 'assets/default-profile-pic.jpg'">
       <div class="chat-info">
         <div class="chat-name">{{ chat.name }}</div>
@@ -19,11 +19,4 @@ export class ChatItemComponent {
   // tslint:disable-next-line:no-input-rename
   @Input('item')
   chat: GetChats.Chats;
-
-  @Output()
-  select = new EventEmitter<string>();
-
-  selectChat() {
-    this.select.emit(this.chat.id);
-  }
 }

src/app/chats-lister/components/chats-list/chats-list.component.ts
@@ -1,14 +1,17 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {GetChats} from '../../../../graphql';
+import {SelectableListDirective} from 'ngx-selectable-list';
 
 @Component({
   selector: 'app-chats-list',
   template: `
     <mat-list>
       <mat-list-item *ngFor="let chat of chats">
-        <app-chat-item [item]="chat" (select)="selectChat($event)"></app-chat-item>
+        <app-chat-item [item]="chat"
+                       libSelectableItem></app-chat-item>
       </mat-list-item>
     </mat-list>
+    <ng-content *ngIf="selectableListDirective.selecting"></ng-content>
   `,
   styleUrls: ['chats-list.component.scss'],
 })
@@ -17,12 +20,5 @@ export class ChatsListComponent {
   @Input('items')
   chats: GetChats.Chats[];
 
-  @Output()
-  select = new EventEmitter<string>();
-
-  constructor() {}
-
-  selectChat(id: string) {
-    this.select.emit(id);
-  }
+  constructor(public selectableListDirective: SelectableListDirective) {}
 }

src/app/chats-lister/containers/chats/chats.component.ts
@@ -28,9 +28,13 @@ import {Router} from '@angular/router';
       </button>
     </mat-menu>
 
-    <app-chats-list [items]="chats$ | async" (select)="goToChat($event)"></app-chats-list>
+    <app-chats-list [items]="chats$ | async"
+                    libSelectableList="both"
+                    (single)="goToChat($event)" (multiple)="deleteChats($event)" (isSelecting)="isSelecting = $event">
+      <app-confirm-selection #confirmSelection></app-confirm-selection>
+    </app-chats-list>
 
-    <button class="chat-button" mat-fab color="secondary">
+    <button *ngIf="!isSelecting" class="chat-button" mat-fab color="secondary">
       <mat-icon aria-label="Icon-button with a + icon">chat</mat-icon>
     </button>
   `,
@@ -38,6 +42,7 @@ import {Router} from '@angular/router';
 })
 export class ChatsComponent implements OnInit {
   chats$: Observable<GetChats.Chats[]>;
+  isSelecting = false;
 
   constructor(private chatsService: ChatsService,
               private router: Router) {
@@ -49,5 +54,11 @@ export class ChatsComponent implements OnInit {
 
   goToChat(chatId: string) {
     this.router.navigate(['/chat', chatId]);
+  }
+
+  deleteChats(chatIds: string[]) {
+    chatIds.forEach(chatId => {
+      this.chatsService.removeChat(chatId).subscribe();
+    });
   }
 }